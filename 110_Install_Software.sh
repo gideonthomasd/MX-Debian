2d1ec6698e6862a0b1fa4d5a0cb345b5797fe441
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-02-27 16:34:35 +0000

    Update 110_Install_Software.sh

--- 110_Install_Software.sh
+++ 110_Install_Software.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 sudo apt install -y i3 dmenu fonts-font-awesome fonts-fork-awesome xfce4-terminal thunar gdebi polybar feh pulsemixer neofetch sxhkd curl dmenu rofi pcmanfm picom lxtask lxappearance lxpolkit aptitude moc lxterminal geany
-sudo apt install -y make gcc dunst libx11-dev libxft-dev libxinerama-dev fonts-noto-mono calcurse meson cmake libpulse-dev libcxxopts-dev fonts-jetbrains-mono i3lock-fancy pamixer
+sudo apt install -y make gcc dunst libx11-dev libxft-dev libxinerama-dev fonts-noto-mono calcurse meson cmake libpulse-dev libcxxopts-dev fonts-jetbrains-mono i3lock-fancy htop
+sudo apt install -y pamixer
