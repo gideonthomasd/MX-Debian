9d6b106f00ec72aa65fb0d39480202e67ed593cf
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-03-23 18:52:05 +0000

    Update load_layout.sh

--- herbstluftwm/load_layout.sh
+++ herbstluftwm/load_layout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SEL="$(ls ~/.config/herbstluftwm/layouts/ | dmenu -i -fn Monaco -l 5 -g 5 -p "Which Layout?: " -nb '#222222' -sb '#181818' -sf '#10713c' -nf '#b8b8b8')"
+SEL="$(ls ~/.config/herbstluftwm/layouts/ | dmenu -fn "Noto Sans Regular-16" -p "Which Layout?: " )"
 herbstclient load "$(cat ~/.config/herbstluftwm/layouts/$SEL)"
